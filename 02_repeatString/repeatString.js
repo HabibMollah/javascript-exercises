5299c03c684e012c94aebe5b91482b38d59f4e5a
Habibur Rahman <hrmollah2002@gmail.com>
2023-01-28 15:01:21 +0600

    3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  let i = 0;
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    while (i < num) {
+      str += string;
+      i++;
+    }
+    return str;
+  }
 };
-
+console.log(repeatString("hey", 0));
 // Do not edit below this line
 module.exports = repeatString;
