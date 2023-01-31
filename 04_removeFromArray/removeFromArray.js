6a41bc9a0b9cd2422dc40b91c86d5bb22a934dd4
Habibur Rahman <hrmollah2002@gmail.com>
2023-01-31 18:51:21 +0600

    4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr, ...val) => {
+  console.log(val);
+  return arr.filter((item) => item !== val);
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  let sum = 0;
+  //check for non-number parameters
+  if (typeof startNum !== typeof 1 || typeof endNum !== typeof 1) {
+    sum = "ERROR";
+  } else if (endNum < startNum) {
+    while (endNum <= startNum) {
+      sum += endNum;
+      endNum++;
+    }
+  } else if (startNum < 0 || endNum < 0) {
+    sum = "ERROR";
+  } else {
+    while (startNum <= endNum) {
+      sum += startNum;
+      startNum++;
+    }
+  }
+  return sum;
 };
+console.log(sumAll(4, "sjh"));
 
 // Do not edit below this line
 module.exports = sumAll;
